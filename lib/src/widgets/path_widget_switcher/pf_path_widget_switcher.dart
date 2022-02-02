6597f269d10d99dc947ee09edd179cf97cef7039
ojm-it <80984705+ojm-it@users.noreply.github.com>
2022-02-02 17:24:59 +0100

    Fixed fromVRouterData Replaced AnimatedSwitcher with a builder

--- lib/src/widgets/path_widget_switcher/pf_path_widget_switcher.dart
+++ lib/src/widgets/path_widget_switcher/pf_path_widget_switcher.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:vrouter/vrouter.dart';
 import 'package:vrouter_x/src/widgets/path_widget_switcher/path_widget.dart';
 import 'package:vrouter_x/src/widgets/path_widget_switcher/path_widget_switcher.dart';
@@ -12,13 +13,11 @@ class PfPathWidgetSwitcher extends StatelessWidget
   PfPathWidgetSwitcher({
     Key? key,
     required this.pathWidgets,
-    this.duration = const Duration(milliseconds: 400),
-    this.transitionBuilder = AnimatedSwitcher.defaultTransitionBuilder,
+    this.builder = PathWidgetSwitcher.defaultBuilder,
     this.caseSensitive = false,
     this.preferredSize = const Size.fromHeight(kToolbarHeight),
-  }) : super(key: key) {
-    getVRouterData = (context) => context.vRouter;
-  }
+  })  : vRouterDataOption = none(),
+        super(key: key);
 
   /// Use this constructor when you want to manually pass-in the current
   /// vRouterData.
@@ -26,22 +25,11 @@ class PfPathWidgetSwitcher extends StatelessWidget
     Key? key,
     required VRouterData vRouterData,
     required this.pathWidgets,
-    this.duration = const Duration(milliseconds: 400),
-    this.transitionBuilder = AnimatedSwitcher.defaultTransitionBuilder,
+    this.builder = PathWidgetSwitcher.defaultBuilder,
     this.caseSensitive = false,
     this.preferredSize = const Size.fromHeight(kToolbarHeight),
-  }) : super(key: key) {
-    getVRouterData = (context) => vRouterData;
-  }
-
-  /// See [PathWidgetSwitcher.getVRouterData]
-  late final VRouterData Function(BuildContext context) getVRouterData;
-
-  /// See [PathWidgetSwitcher.duration]
-  final Duration duration;
-
-  /// See [PathWidgetSwitcher.transitionBuilder]
-  final AnimatedSwitcherTransitionBuilder transitionBuilder;
+  })  : vRouterDataOption = some(vRouterData),
+        super(key: key);
 
   /// See [PathWidgetSwitcher.pathWidgets]
   final List<PathWidget> pathWidgets;
@@ -49,17 +37,30 @@ class PfPathWidgetSwitcher extends StatelessWidget
   /// See [PathWidgetSwitcher.caseSensitive]
   final bool caseSensitive;
 
+  /// This will hold some() when the vRouterData has been provided in the
+  /// constructor, and none() otherwise.
+  final Option<VRouterData> vRouterDataOption;
+
+  /// See [PathWidgetSwitcher.builder]
+  final Widget Function(BuildContext context, Widget child) builder;
+
   @override
   final Size preferredSize;
 
   @override
   Widget build(BuildContext context) {
-    return PathWidgetSwitcher(
-      key: key,
-      pathWidgets: pathWidgets,
-      duration: duration,
-      transitionBuilder: transitionBuilder,
-      caseSensitive: caseSensitive,
+    return vRouterDataOption.match(
+      (vRouterData) => PathWidgetSwitcher.fromVRouterData(
+        vRouterData: vRouterData,
+        pathWidgets: pathWidgets,
+        builder: builder,
+        caseSensitive: caseSensitive,
+      ),
+      () => PathWidgetSwitcher(
+        pathWidgets: pathWidgets,
+        builder: builder,
+        caseSensitive: caseSensitive,
+      ),
     );
   }
 }
