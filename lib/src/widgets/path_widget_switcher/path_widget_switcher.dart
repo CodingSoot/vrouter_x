6f46652d1f8ad5b81e146c74e0c21cf61fd49339
ojm-it <80984705+ojm-it@users.noreply.github.com>
2022-02-02 17:22:42 +0100

    Added documentation Replaced AnimatedSwitcher with a builder Replaced getVRouterData with vRouterDataOption

--- lib/src/widgets/path_widget_switcher/path_widget_switcher.dart
+++ lib/src/widgets/path_widget_switcher/path_widget_switcher.dart
@@ -5,18 +5,40 @@ import 'package:vrouter/vrouter.dart';
 import 'package:vrouter_x/src/_core/errors.dart';
 import 'package:vrouter_x/src/widgets/path_widget_switcher/path_widget.dart';
 
+/// Declaratively switch child widgets based on the current vRouterData (Or based on a vRouterData you provide).
+///
+/// This is useful in 2 primary use cases:
+/// - When you have scaffolding around your Navigator, like a SideBar or a TitleBar and you would like it to react to location changes
+/// - When multiple paths resolve to the same Page and you want to move subsequent routing further down the tree
+///
+/// ### Example :
+/// ```dart
+/// class SideBar extends StatelessWidget {
+///     Widget build(_){
+///      return PathWidgetSwitcher(
+///         pathWidgets: [
+///           PathWidget(
+///             path: '/', //or MainRoute.routeInfo.path!
+///             builder: (path) => const MainMenu(),
+///           ),
+///           PathWidget(
+///             path: '/profile', //or ProfileRoute.routeInfo.path!
+///             builder: (path) => const ProfileMenu(),
+///           ),
+///         ]);
+///     }
+/// }
+/// ```
 class PathWidgetSwitcher extends StatelessWidget {
   /// Use this constructor when you want to automatically extract the current
   /// vRouterData from the context.
   PathWidgetSwitcher({
     Key? key,
     required this.pathWidgets,
-    this.duration = const Duration(milliseconds: 400),
-    this.transitionBuilder = AnimatedSwitcher.defaultTransitionBuilder,
+    this.builder = defaultBuilder,
     this.caseSensitive = false,
-  }) : super(key: key) {
-    getVRouterData = (context) => context.vRouter;
-  }
+  })  : vRouterDataOption = none(),
+        super(key: key);
 
   /// Use this constructor when you want to manually pass-in the current
   /// vRouterData.
@@ -24,20 +46,10 @@ class PathWidgetSwitcher extends StatelessWidget {
     Key? key,
     required VRouterData vRouterData,
     required this.pathWidgets,
-    this.duration = const Duration(milliseconds: 400),
-    this.transitionBuilder = AnimatedSwitcher.defaultTransitionBuilder,
+    this.builder = defaultBuilder,
     this.caseSensitive = false,
-  }) : super(key: key) {
-    getVRouterData = (context) => vRouterData;
-  }
-
-  late final VRouterData Function(BuildContext context) getVRouterData;
-
-  /// Duration of the animation when switching widgets
-  final Duration duration;
-
-  /// Allows custom animations when switching widgets, defaults to `AnimatedSwitcher.defaultTransitionBuilder`
-  final AnimatedSwitcherTransitionBuilder transitionBuilder;
+  })  : vRouterDataOption = some(vRouterData),
+        super(key: key);
 
   /// A list of all matching paths and their associated builder method.
   final List<PathWidget> pathWidgets;
@@ -46,9 +58,25 @@ class PathWidgetSwitcher extends StatelessWidget {
   /// per path basis.
   final bool caseSensitive;
 
-  /// Find a matching RoutedWidget for the current vRouterData.
-  Option<PathWidget> findPathWidget(VRouterData vRouterData) {
-    //We need only the path when matching (without the query params or the hash)
+  /// This will hold some() when the vRouterData has been provided in the
+  /// constructor, and none() otherwise.
+  final Option<VRouterData> vRouterDataOption;
+
+  /// The builder for the current pathWidget's child.
+  ///
+  /// You can for example use an [AnimatedSwitcher], or a package such as
+  /// animated_size_and_fade to animate the transition between different
+  /// children when the current pathWidget changes.
+  ///
+  /// Defaults to [defaultBuilder].
+  final Widget Function(BuildContext context, Widget child) builder;
+
+  static Widget defaultBuilder(BuildContext context, Widget child) => child;
+
+  /// Find a matching PathWidget for the current vRouterData.
+  Option<PathWidget> _findPathWidget(VRouterData vRouterData) {
+    // When matching, we only need the path (without the query params or the
+    // hash)
     final currentPath = vRouterData.path ?? '';
 
     List<PathWidget> matches = pathWidgets.where((pathWidget) {
@@ -68,12 +96,12 @@ class PathWidgetSwitcher extends StatelessWidget {
       return regEx.hasMatch(currentPath);
     }).toList();
 
-    final matchOption = getMostSpecificMatch(currentPath, matches);
+    final matchOption = _getMostSpecificMatch(currentPath, matches);
     return matchOption;
   }
 
   /// Given a list of possible matches, return the most exact one.
-  Option<PathWidget> getMostSpecificMatch(
+  Option<PathWidget> _getMostSpecificMatch(
     String currentPath,
     List<PathWidget> matches,
   ) {
@@ -121,19 +149,15 @@ class PathWidgetSwitcher extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final vRouterData = getVRouterData(context);
+    final vRouterData = vRouterDataOption.getOrElse(() => context.vRouter);
 
-    final pathWidgetOption = findPathWidget(vRouterData);
+    final pathWidgetOption = _findPathWidget(vRouterData);
 
     final child = pathWidgetOption.match(
       (pathWidget) => pathWidget.builder(pathWidget.path),
       () => throw UnknownPathWidgetError(path: vRouterData.path ?? ''),
     );
 
-    return AnimatedSwitcher(
-      duration: duration,
-      child: child,
-      transitionBuilder: transitionBuilder,
-    );
+    return builder(context, child);
   }
 }
