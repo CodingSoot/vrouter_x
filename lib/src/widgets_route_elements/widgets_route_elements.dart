706b4ad896b47eb300a1d82e1fcb02dd991aacbb
ojm-it <80984705+ojm-it@users.noreply.github.com>
2022-02-09 17:18:23 +0100

    export StickyConfig and StickyQueryParamsScope

--- lib/src/widgets_route_elements/widgets_route_elements.dart
+++ lib/src/widgets_route_elements/widgets_route_elements.dart
@@ -7,3 +7,7 @@ export 'common/initial_go_to_resolver.dart' show InitialGoToResolver;
 export 'tab_bar/vx_tab_bar.dart' show VxTabBar;
 
 export 'tabs_scaffold/vx_tabs_scaffold.dart' show VxTabsScaffold;
+
+export 'sticky_query_params/sticky_query_param.dart' show StickyConfig;
+export 'sticky_query_params/sticky_query_params_scope.dart'
+    show StickyQueryParamsScope;
